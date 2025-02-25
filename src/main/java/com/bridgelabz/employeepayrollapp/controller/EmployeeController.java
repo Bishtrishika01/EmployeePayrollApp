f1ebef9d4628ddd6cbc94fd7933fb3b2c1de8215
Bishtrishika01 <bishtrishika66@gamil.com>
2025-02-25 10:55:37 +0530

    [Rishika] Add. UpdateController

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -2,7 +2,10 @@ package com.bridgelabz.employeepayrollapp.controller;
 
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,35 +13,40 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    private final EmployeeService employeeService;
-
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
-
+    Logger logger = Logger.getLogger(EmployeeController.class);
+    @Autowired
+    EmployeeService employeeService;
+    //To get all the employees
     @GetMapping
-    public List<Employee> getAllEmployees() {
-        return employeeService.getAllEmployees();
+    public List<Employee> getAllEmployees(){
+        logger.info("All employee list end point called");
+        return employeeService.getAllEmployee();
     }
 
-    @GetMapping("/{id}")
-    public Optional<Employee> getEmployeeById(@PathVariable Long id) {
+    //TO get employee with id
+    @GetMapping("/get/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable Long id){
+        logger.info("specific employee list end point called");
         return employeeService.getEmployeeById(id);
     }
 
     @PostMapping("/create")
-    public Employee addEmployee(@RequestBody Employee employee) {
+    public Employee addEmployee(@RequestBody Employee employee){
+        logger.info("member created");
         return employeeService.addEmployee(employee);
     }
 
+    //Update the employee
     @PutMapping("/update/{id}")
-    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee) {
-        return employeeService.updateEmployee(id, updatedEmployee);
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        logger.info("Employee updation endpoint called");
+        return employeeService.updateEmployee(id,employee);
     }
 
+    //Delete the employee
     @DeleteMapping("/delete/{id}")
-    public String deleteEmployee(@PathVariable Long id) {
+    public void deleteEmployee(@PathVariable Long id){
+        logger.info("employee deletion endpint called");
         employeeService.deleteEmployee(id);
-        return "Employee deleted successfully";
     }
 }

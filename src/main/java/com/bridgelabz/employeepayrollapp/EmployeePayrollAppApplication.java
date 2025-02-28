49d7c9bc1cec4d14f60f13df7cef55aac1c146f6
Bishtrishika01 <bishtrishika66@gamil.com>
2025-02-28 19:17:29 +0530

    [Rishika] Add. Update Employee Payroll Application

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,16 +2,13 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @SpringBootApplication
+@EnableJpaRepositories("com.bridgelabz.employeepayrollapp.repository")
 public class EmployeePayrollAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		log.info("Employee Payroll App started");
 	}
-
 }
+
